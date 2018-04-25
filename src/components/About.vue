11c00aa14e9f4df070dbf7539f73b36d08fbf662
Ervin <bbuchala93@gmail.com>
2018-04-25 12:06:31 +0200

    Carousel in about view.

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,67 +1,90 @@
 <template>
-<div>
-  <v-container grid-list-xl text-xs-center style="min-height: 0;" hidden-sm-and-down>
-        <v-layout row wrap
-        >
-          <v-flex v-for="(item, i) in items" v-bind:key="i" xs4>
+  <div>
+    <v-container grid-list-xl text-xs-center style="min-height: 0;" hidden-sm-and-down>
+      <v-carousel style="height: 100%" >
+        <v-carousel-item v-for="(slide, i) in slides" :src="slide.imageSrc" :key="i">
+          <v-jumbotron dark>
+            <v-container fill-height>
+              <v-layout align-center>
+                <v-flex>
+                  <h3>
+                    <router-link
+                      :to="slide.link"
+                      tag="span"
+                      class="display-3"
+                      style="cursor: pointer"
+                      >
+                      {{ slide.title }}
+                    </router-link>
+                  </h3>
+                  <span class="subheading">{{ slide.text }}</span>
+                </v-flex>
+              </v-layout>
+            </v-container>
+          </v-jumbotron>
+        </v-carousel-item>
+      </v-carousel>
+    </v-container>
+    <v-container grid-list-xl text-xs-center style="min-height: 0;" hidden-sm-and-down>
+    <v-layout row wrap>
+      <v-flex v-for="(item, i) in cardItems" v-bind:key="i" xs4>
+        <v-card color="primary" class="white--text">
+            <v-card-media
+              :src="item.imageSrc"
+              :alt="item.imageAltText"
+              height="300px"/>
+            <v-card-actions>
+            <v-card-title primary-title class="display-1 styledHeader" v-html="item.header">>
+            </v-card-title>
+            <v-spacer></v-spacer>
+            <v-btn icon @click.native="item.expanded = !item.expanded">
+              <v-icon>{{ item.expanded ? 'keyboard_arrow_up' : 'keyboard_arrow_down' }}</v-icon>
+            </v-btn>
+          </v-card-actions>
+          <v-slide-y-transition>
+            <v-card-text v-show="item.expanded" class="subheading">
+              <p v-for="(paragraph, j) in item.paragraphs" v-bind:key="j">
+                  {{ paragraph }}
+              </p>
+          </v-card-text>
+          </v-slide-y-transition>
+        </v-card>
+      </v-flex>
+    </v-layout>
+    </v-container>
+    <v-container grid-list-xl text-xs-center style="min-height: 0;" hidden-md-and-up>
+        <v-layout row wrap>
+          <v-flex v-for="(item, i) in cardItems" v-bind:key="i" xs12>
             <v-card color="primary" class="white--text">
-                <v-card-media
-                  :src="item.imagePath"
-                  :alt="item.imageAltText"
-                  height="300px"/>
-                <v-card-actions>
-                <v-card-title primary-title class="display-1 styledHeader" v-html="item.header">>
+              <v-card-media :src="item.imageSrc" height="250px" :alt="item.imageAltText"></v-card-media>
+              <v-card-actions>
+                <v-card-title primary-title class="headline styledHeader" v-html="item.header">>
                 </v-card-title>
                 <v-spacer></v-spacer>
                 <v-btn icon @click.native="item.expanded = !item.expanded">
-                  <v-icon>{{ item.expanded ? 'keyboard_arrow_up' : 'keyboard_arrow_down' }}</v-icon>
+                  <v-icon>{{ item.expanded ? 'keyboard_arrow_down' : 'keyboard_arrow_up' }}</v-icon>
                 </v-btn>
               </v-card-actions>
-              <v-slide-y-transition>
-                <v-card-text v-show="item.expanded" class="subheading">
-                  <p v-for="(paragraph, j) in item.paragraphs" v-bind:key="j">
-                     {{ paragraph }}
-                  </p>
+              <v-card-text v-show="item.expanded" class="subheading">
+                <p v-for="(paragraph, j) in item.paragraphs" v-bind:key="j">
+                  {{ paragraph }}
+                </p>
               </v-card-text>
-              </v-slide-y-transition>
             </v-card>
           </v-flex>
         </v-layout>
-  </v-container>
-    <v-container grid-list-xl text-xs-center style="min-height: 0;" hidden-md-and-up>
-       <v-layout row wrap
-            >
-              <v-flex v-for="(item, i) in items" v-bind:key="i" xs12>
-                <v-card color="primary" class="white--text">
-                  <v-card-media :src="item.imagePath" height="250px" :alt="item.imageAltText"></v-card-media>
-                  <v-card-actions>
-                    <v-card-title primary-title class="headline styledHeader" v-html="item.header">>
-                    </v-card-title>
-                    <v-spacer></v-spacer>
-                    <v-btn icon @click.native="item.expanded = !item.expanded">
-                      <v-icon>{{ item.expanded ? 'keyboard_arrow_down' : 'keyboard_arrow_up' }}</v-icon>
-                    </v-btn>
-                  </v-card-actions>
-                  <v-card-text v-show="item.expanded" class="subheading">
-                    <p v-for="(paragraph, j) in item.paragraphs" v-bind:key="j">
-                      {{ paragraph }}
-                    </p>
-                  </v-card-text>
-                </v-card>
-              </v-flex>
-            </v-layout>
-  </v-container>
-</div>
+    </v-container>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'About',
   data: () => ({
-    items: [
+    cardItems: [
       {
         header: 'Who is a seiyuu?',
-        imagePath: '/static/rie.jpg',
+        imageSrc: '/static/rie.jpg',
         imageAltText: 'Seiyuu Kugimiya Rie during recording',
         expanded: false,
         paragraphs: [
@@ -71,13 +94,21 @@ export default {
       },
       {
         header: 'About project',
-        imagePath: '/static/lain.jpg',
+        imageSrc: '/static/lain.jpg',
         imageAltText: 'About Seiyuu.Moe project',
         expanded: false,
         paragraphs: [
           'Hello, I am developer from Silesia region, Poland. This website is a result of connecting my hobbies (anime and programming), non-profit project for general use.',
           'Source code can be found in link on the sidebar. Have in mind, that website is in its alpha stage and still under development. Suggestion or bugs can be issued via Github.'
         ]
+      }
+    ],
+    slides: [
+      {
+        imageSrc: '/static/questionMark.png',
+        title: 'Get Started',
+        text: 'Select and compare seiyuu.',
+        link: '/'
       }
     ]
   })
