75fbf753d62e1a1e8d065e449722832d02e1af1e
Ervie <bbuchala93@gmail.com>
2018-08-17 16:58:36 +0200

    Change rendering to SPA

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ const pkg = require('./package')
 const nodeExternals = require('webpack-node-externals')
 
 module.exports = {
-  mode: 'universal',
+  mode: 'spa',
 
   /*
   ** Headers of the page
