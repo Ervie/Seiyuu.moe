891cd02cf6de36f96cdf5353a5b6c78a379893ad
Ervie <bbuchala93@gmail.com>
2020-10-01 19:50:06 +0200

    First lambda pair without implementation

---
+++ API/SeiyuuMoe.MalBackgroundJobs.Lambda/Base/BaseSqsLambda.cs
@@ -0,0 +1,28 @@
+﻿using Amazon.Lambda.SQSEvents;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using static Amazon.Lambda.SQSEvents.SQSEvent;
+
+namespace SeiyuuMoe.MalBackgroundJobs.Lambda.Base
+{
+	public abstract class BaseSqsLambda
+	{
+		public async Task InvokeAsync(SQSEvent @event)
+		{
+			XRayTracing.Configure();
+
+			try
+			{
+				var sqsMessage = @event.Records.Single();
+				await HandleAsync(sqsMessage);
+			}
+			catch (Exception)
+			{
+				throw;
+			}
+		}
+
+		protected abstract Task HandleAsync(SQSMessage message);
+	}
+}
---
+++ API/SeiyuuMoe.MalBackgroundJobs.Lambda/Function/UpdateAnimeLambda.cs
@@ -0,0 +1,15 @@
+﻿using SeiyuuMoe.MalBackgroundJobs.Lambda.Base;
+using System;
+using System.Threading.Tasks;
+using static Amazon.Lambda.SQSEvents.SQSEvent;
+
+namespace SeiyuuMoe.MalBackgroundJobs.Lambda.Function
+{
+	internal class UpdateAnimeLambda : BaseSqsLambda
+	{
+		protected async override Task HandleAsync(SQSMessage message)
+		{
+			Console.WriteLine($"UpdateAnimeLambda was imvoked with message {message}");
+		}
+	}
+}
